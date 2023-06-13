d64e14c22fabe67687360ad8e4e370177b5b94f7
Blytte1 <blytte@gmail.com>
2023-06-13 14:21:16 -0300

    Initial Commit

---
+++ LifePlus/LifePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifePlusApp.swift
+//  LifePlus
+//
+//  Created by Roberto Mascarenhas on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LifePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
